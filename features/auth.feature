fa9ea15669be36226d74bb7bc1507f5c169cd8a6
Jace Ju <jaceju@gmail.com>
2015-08-06 01:12:41 +0800

    New scenario

--- features/auth.feature
+++ features/auth.feature
@@ -4,3 +4,9 @@ Feature: User is signed in
         Given a user whose name is "jaceju"
         When user attempts to sign in
         Then user should be sign in
+
+    Scenario: User sign out
+        Given a user whose name is "jaceju"
+        And user is signed in
+        When user attempts to sign out
+        Then user should be sign out
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -30,11 +30,15 @@ class FeatureContext extends MinkContext implements KernelAwareContext, SnippetA
 
     /**
      * @Given a user whose name is :name
+     * @param $name
      */
     public function aUserWhoseNameIs($name)
     {
         $this->user = factory(App\User::class)->create([
             'name' => $name,
+        ]);
+        $this->seeInDatabase('users', [
+            'name' => $this->user->name,
         ]);
     }
 
@@ -52,8 +56,30 @@ class FeatureContext extends MinkContext implements KernelAwareContext, SnippetA
     public function userShouldBeSignIn()
     {
         $this->assertTrue(Auth::check());
-        $this->seeInDatabase('users', [
-            'name' => $this->user->name,
-        ]);
+    }
+
+    /**
+     * @Given user is signed in
+     */
+    public function userIsSignedIn()
+    {
+        $this->userAttemptsToSignIn();
+        $this->userShouldBeSignIn();
+    }
+
+    /**
+     * @When user attempts to sign out
+     */
+    public function userAttemptsToSignOut()
+    {
+        Auth::logout();
+    }
+
+    /**
+     * @Then user should be sign out
+     */
+    public function userShouldBeSignOut()
+    {
+        $this->assertFalse(Auth::check());
     }
 }
